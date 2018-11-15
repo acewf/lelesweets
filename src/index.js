45db9250456d2493557b64188fd0f9fd67540de6
Pedro Martins <ace@pedros-mbp.mynet>
2018-11-15 22:16:22 +0000

    update virtual host

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.VIRTUAL_PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello LeleSweets World!'))
+app.get('/', (req, res) => res.send(`Hello LeleSweets World! ${Date.now()}`))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
